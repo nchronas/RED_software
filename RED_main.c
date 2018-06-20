38ed22909ecbf30525ec4fd755a450ed94b5b47b
nchronas <nchronas@gmail.com>
2018-06-20 17:05:12 +0200

    Working test system

---
+++ RED_main.c
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2015-2017, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ======== uartecho.c ========
+ */
+#include <stdint.h>
+#include <stddef.h>
+
+/* Driver Header files */
+#include <ti/drivers/GPIO.h>
+#include <ti/drivers/UART.h>
+
+#include <ti/drivers/ADC.h>
+#include <ti/drivers/ADCBuf.h>
+#include <ti/drivers/GPIO.h>
+#include <ti/drivers/I2C.h>
+#include <ti/drivers/PWM.h>
+#include <ti/drivers/SPI.h>
+#include <ti/drivers/UART.h>
+#include <ti/drivers/Watchdog.h>
+#define __MSP432P401R__
+#include <ti/devices/msp432p4xx/driverlib/uart.h>
+#include <ti/drivers/uart/UARTMSP432.h>
+
+
+#include "PQ9_bus_engine.h"
+#include "queue.h"
+
+#include "RED_Board.h"
+
+bool start_flag = false;
+
+/*
+ *  ======== mainThread ========
+ */
+void *mainThread(void *arg0)
+{
+
+    /* Call driver init functions */
+    GPIO_init();
+    UART_init();
+    Timer_init();
+
+    /*PQ9 services start*/
+    pkt_pool_INIT();
+    device_init();
+    queueInit();
+
+    start_flag = true;
+
+    /* Loop forever echoing */
+    while (1) {
+
+        update_device(OBC_MON_DEV_ID);
+        usleep(1);
+
+        update_device(OBC_TEMP_DEV_ID);
+        usleep(1);
+
+        usleep(100);
+
+
+    }
+}
+
+/*  ======== ecssThread ========
+ *  This thread runs on a higher priority, since wdg pin
+ *  has to be ready for master.
+ */
+void *pqReceiveThread(void *arg0)
+{
+
+    while(!start_flag) {
+        usleep(1000);
+    }
+
+    /* Loop forever */
+    while (1) {
+         import_pkt();
+         usleep(1);
+    }
+
+    return (NULL);
+}
+
+void *pqTransmitThread(void *arg0)
+{
+
+    while(!start_flag) {
+        usleep(1000);
+    }
+
+    /* Loop forever */
+    while (1) {
+         export_pkt();
+         usleep(1);
+    }
+
+    return (NULL);
+}
+
+void *dbgThread(void *arg0)
+{
+
+    while(!start_flag) {
+        usleep(1000);
+    }
+
+    /* Loop forever */
+    while (1) {
+
+         usleep(1);
+    }
+
+    return (NULL);
+}
