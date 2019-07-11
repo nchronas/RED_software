a6c31e56b4fdf037496a6cb69a80009ab16535d5
nchronas <nchronas@gmail.com>
2019-07-11 11:14:10 +0200

    updating RED Board

--- RED_main.c
+++ RED_main.c
@@ -55,6 +55,7 @@
 
 #include "PQ9_bus_engine.h"
 #include "queue.h"
+#include "subsystem.h"
 
 #include "osal.h"
 
@@ -64,6 +65,55 @@ extern UART_Handle uart_pq9_bus;
 extern UART_Handle uart_dbg_bus;
 
 bool start_flag = false;
+
+
+#define HLDLC_START_FLAG        0x7E
+#define HLDLC_CONTROL_FLAG      0x7D
+#define HLDLC_STOP_FLAG         0x7C
+
+uint8_t tx_count, tx_size, tx_buf[255];
+uint8_t rx_count, rx_size, rx_buf[255];
+bool ctrl_flag, strt_flag = false;
+uint8_t resp54[3];
+uint8_t res;
+
+uint8_t pq_rx_buf[300];
+uint16_t pq_rx_count, pq_size;
+bool pq_rx_flag;
+//uint16_t pq_rx_addr_cnt = 0;
+//uint16_t pq_rx_byte_cnt = 0;
+
+void HLDLC_frame(uint8_t *buf_in, uint8_t *buf_out, uint16_t size_in, uint16_t *size_out) {
+
+    uint16_t cnt = 2;
+
+    for(uint16_t i = 0; i < size_in; i++) {
+        if(i == 0) {
+            buf_out[0] = HLDLC_START_FLAG;
+            buf_out[1] = buf_in[0];
+        } else if(i == (size_in) - 1) {
+            if(buf_in[i] == HLDLC_START_FLAG) {
+                buf_out[cnt++] = HLDLC_CONTROL_FLAG;
+                buf_out[cnt++] = 0x5E;
+            } else if(buf_in[i] == HLDLC_CONTROL_FLAG) {
+                buf_out[cnt++] = HLDLC_CONTROL_FLAG;
+                buf_out[cnt++] = 0x5D;
+            } else { buf_out[cnt++] = buf_in[i]; }
+            //buf_out[cnt++] = HLDLC_START_FLAG;
+            *size_out = cnt;
+            return ;
+        } else if(buf_in[i] == HLDLC_START_FLAG) {
+            buf_out[cnt++] = HLDLC_CONTROL_FLAG;
+            buf_out[cnt++] = 0x5E;
+        } else if(buf_in[i] == HLDLC_CONTROL_FLAG) {
+            buf_out[cnt++] = HLDLC_CONTROL_FLAG;
+            buf_out[cnt++] = 0x5D;
+        } else {
+            buf_out[cnt++] = buf_in[i];
+        }
+
+    }
+}
 
 /*
  *  ======== mainThread ========
@@ -75,7 +125,7 @@ void *mainThread(void *arg0)
     GPIO_init();
     UART_init();
     Timer_init();
-    Watchdog_init();
+   // Watchdog_init();
 
     /*PQ9 services start*/
     pkt_pool_INIT();
@@ -100,6 +150,9 @@ void *mainThread(void *arg0)
     }
 }
 
+
+uint8_t test_loop = 0;
+
 /*  ======== ecssThread ========
  *  This thread runs on a higher priority, since wdg pin
  *  has to be ready for master.
@@ -111,9 +164,126 @@ void *pqReceiveThread(void *arg0)
         usleep(1000);
     }
 
+    uint8_t *ts = &test_loop;
+
     /* Loop forever */
     while (1) {
-         import_pkt();
+
+          while(test_loop) {
+
+          }
+
+          bool res_uart = false;
+
+          pq9_pkt *pkt_rx;
+          uint16_t uart_size = 0;
+          uint16_t pq_size = 0;
+          char res_rx = 0;
+
+          //res_uart = HAL_uart_rx(0, &ud.uart_rx_buf, &uart_size);
+
+          do {
+                res_rx = UART_read(uart_dbg_bus, resp54, 1);
+                if(res_rx > 0) {
+                  if(resp54[0] == HLDLC_START_FLAG) {
+                    strt_flag = true;
+                  } else if(resp54[0] == HLDLC_CONTROL_FLAG) {
+                    ctrl_flag = true;
+                  } else if(strt_flag) {
+                     strt_flag = false;
+                     tx_buf[0] = resp54[0];
+                     tx_count = 1;
+                  } else if(ctrl_flag) {
+                     ctrl_flag = false;
+                     if(resp54[0] == 0x5D) {
+                       tx_buf[tx_count] = 0x7D;
+                       tx_count++;
+                     } else if(resp54[0] == 0x5E) {
+                       tx_buf[tx_count] = 0x7E;
+                       tx_count++;
+                     }
+                  } else if(tx_count == 1) {
+                    tx_buf[tx_count] = resp54[0];
+                    tx_size = resp54[0] + 5;
+                    tx_count++;
+                  } else if(tx_count > 0 && tx_count < tx_size - 1) {
+                    tx_buf[tx_count] = resp54[0];
+                    tx_count++;
+                  } else if(tx_count > 0 && tx_count == tx_size - 1) {
+                    tx_buf[tx_count] = resp54[0];
+                    tx_count++;
+                    if(tx_buf[0] < 127) {
+                      res_uart = true;
+                    }
+                  }
+                }
+              } while(res_rx > 0);
+
+          if(res_uart == true) {
+            res_uart == false;
+            //tx_buf, tx_count
+
+            pkt_rx = get_pkt(pq_size);
+            if(!C_ASSERT(pkt_rx != NULL) == true) {
+                continue ;
+            }
+
+            bool res_unpack_PQ = unpack_PQ9_BUS(tx_buf,
+                                                tx_count,
+                                                pkt_rx);
+            if(pkt_rx->dest_id != SYSTEM_APP_ID) {
+              free_pkt(pkt_rx);
+              continue ;
+            }
+
+            if(res_unpack_PQ == true) {
+
+              route_pkt(pkt_rx);
+            } else {
+              free_pkt(pkt_rx);
+              continue ;
+            }
+
+            free_pkt(pkt_rx);
+
+            //transmit();
+            {
+
+              pq9_pkt *pkt_tx = 0;
+              uint16_t size = 0;
+
+              if((pkt_tx = queuePop(RS_POOL_ID)) ==  NULL) {
+                  continue ;
+              }
+
+
+              bool res = pack_PQ9_BUS(pkt_tx, rx_buf, &size);
+              if(res == false) {
+                free_pkt(pkt_tx);
+                continue ;
+              }
+
+              if(!C_ASSERT(size > 0) == true) {
+                free_pkt(pkt_tx);
+                continue ;
+              }
+
+              //update_pstats_tx_counter(pkt_tx->dest_id);
+              //HAL_uart_tx(0, &ud.uart_tx_buf, size);
+              //Add hldlc
+              //add tx
+              uint8_t buf_rs[255];
+              uint16_t buf_cnt;
+
+              HLDLC_frame(rx_buf, buf_rs, size, &buf_cnt);
+
+              UART_write(uart_dbg_bus, buf_rs, buf_cnt);
+              buf_cnt = 0;
+
+              free_pkt(pkt_tx);
+            }
+
+          }
          usleep(1);
     }
 
@@ -129,8 +299,8 @@ void *pqTransmitThread(void *arg0)
 
     /* Loop forever */
     while (1) {
-         export_pkt();
-         usleep(1);
+         //export_pkt();
+         sleep(100);
     }
 
     return (NULL);
